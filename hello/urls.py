56ecbade68078100faaca4a92620ae49e95026e6
ABIR H. KHANDOKAR <116947694+AKH570@users.noreply.github.com>
2023-01-24 07:26:47 +0600

    class 60 Grouping cartitem variations

--- hello/urls.py
+++ hello/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from welcome import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('welcome.urls')),
+    path('',views.welcome),
 ]
